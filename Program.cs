dd72ac68856dffaee7edc65f7415032f9a7f184b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-23 20:42:09 -0300

    `entregue´´

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// bool exibirMensagem = true;
+int numExibições = 0;
+int maxExibições = 273;
+
+while (numExibições < maxExibições)
+{
+     numExibições = numExibições +1;
+     Console.WriteLine($"Hello, World! #{numExibições} vez de um total de {maxExibições}");
+
+     // if (numExibições >- maxExibições)
+     // {
+     //    exibirMensagem - false;
+     // }
+     
+     // exibirMensagem - numExibições < maxExibições;
+
+}
